a8cdd42ea0a761d0a84c565e247128cede8b58b6
zhrivodkin <zhrivodkin@gmail.com>
2015-01-19 01:55:01 +0400

    templates, model

---
+++ js/models/todo.js
@@ -0,0 +1,18 @@
+var app = app || {};
+// модель задачи
+// ----------
+// Модель задачи имеет атрибуты 'title', 'order' и 'completed'.
+app.Todo = Backbone.Model.extend({
+    // Атрибуты по умолчанию определяют, что у каждой созданной задачи будут ключи
+    // `title` и `completed`.
+    defaults: {
+        title: '',
+        completed: false
+    },
+    // переключение состояния задачи `completed`.
+    toggle: function() {
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+});
